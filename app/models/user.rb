565c7a41f76539664499a018dbe7ecfb6caee741
hoangsonhv <hvsson102@gmail.com>
2021-12-16 08:44:32 +0700

    Chapter 8: Basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ApplicationRecord
+  PROPERTIES = %i(name email password password_confirmation).freeze
   before_save :downcase_email
 
   validates :name, presence: true,
-  length: {maximum: Settings.name_maximun_length}
+    length: {maximum: Settings.name_maximun_length}
   validates :email, presence: true,
-  length: {maximum: Settings.email_maximun_length},
-  format: {with: Settings.email_regex},
-  uniqueness: {case_sensitive: false}
+    length: {maximum: Settings.email_maximun_length},
+    format: {with: Settings.email_regex},
+    uniqueness: {case_sensitive: false}
   validates :password, presence: true,
-  length: {minimum: Settings.password_minimun},
-  allow_nil: true, if: :password
+    length: {minimum: Settings.password_minimun},
+    allow_nil: true, if: :password
   has_secure_password
 
   private
