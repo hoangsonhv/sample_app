6170229b71c3a17a7a9b3df34d742960693181f2
hoangsonhv <hvsson102@gmail.com>
2021-12-16 08:44:32 +0700

    Chapter 11: Account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = t "success"
-      redirect_to @user
+      @user.send_mail_activate
+      flash[:success] = t "mail.success"
+      redirect_to root_path
     else
       flash[:danger] = t "danger"
       render :new
